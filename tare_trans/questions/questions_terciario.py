49f637deedf1fd55ff3b2e79bfc1975acde4019a
VforVitorio <victorvegasobral@yahoo.com>
2024-10-31 11:39:10 +0100

    añadidos archivos por sector

---
+++ tare_trans/questions/questions_terciario.py
@@ -0,0 +1,16 @@
+from typing import List, Dict, Union
+
+
+# Lista de preguntas actualizada con la estructura correcta
+questions_terciario: List[Dict[str, Union[str, List[str]]]] = [
+    {
+        "question": "¿Pregunta específica para SECTOR TERCIARIO   1?",
+        "options": [
+            "Opción 1",
+            "Opción 2",
+            "Opción 3",
+            "Opción 4",
+            "Opción 5"
+        ]
+    },
+]
